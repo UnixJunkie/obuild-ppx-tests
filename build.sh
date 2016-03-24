392397a4f60e7ba7695f917473e279420bc8762f
Francois BERENGER <francois.berenger@inria.fr>
2016-03-24 16:20:08 +0100

    have the dumb ocamlbuild spit out what it is doing

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-ocamlbuild -use-ocamlfind -pkg ppx_deriving.show deriving.byte
-ocamlbuild -use-ocamlfind -pkg ppx_sexp_conv sexp.byte
-ocamlbuild -use-ocamlfind -pkg ppx_bin_prot binprot.byte
+ocamlbuild -classic-display -use-ocamlfind -pkg ppx_deriving.show deriving.byte
+ocamlbuild -classic-display -use-ocamlfind -pkg ppx_sexp_conv sexp.byte
+ocamlbuild -classic-display -use-ocamlfind -pkg ppx_bin_prot binprot.byte
