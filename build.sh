879f3db07214b7d418289d65925455ea115d0f35
Francois BERENGER <francois.berenger@inria.fr>
2016-03-24 16:17:52 +0100

    updated build.sh to build all examples with ocamlbuild

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
-ocamlbuild -clean
-ocamlbuild -classic-display -package deriving deriving.byte
+ocamlbuild -use-ocamlfind -pkg ppx_deriving.show deriving.byte
+ocamlbuild -use-ocamlfind -pkg ppx_sexp_conv sexp.byte
+ocamlbuild -use-ocamlfind -pkg ppx_bin_prot binprot.byte
